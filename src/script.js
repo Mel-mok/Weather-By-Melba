bdf3930f046379606509ae56e54461672bd56705
Melba Mokoena <Melmok2@gmail.com>
2024-05-04 23:15:51 +0200

    update the weather details

--- src/script.js
+++ src/script.js
@@ -1,9 +1,42 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#city-temperature");
-  let temperature = response.data.temperature.current;
   let cityName = document.querySelector(".city-name");
+  let humidity = document.querySelector(".humidity");
+  let wind = document.querySelector(".wind");
+  let description = document.querySelector(".condition");
+  let time = document.querySelector(".date");
+  let date = new Date(response.data.time * 1000);
+
+  let temperature = response.data.temperature.current;
   cityName.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidity.innerHTML = response.data.temperature.humidity;
+  wind.innerHTML = response.data.wind.speed;
+  description.innerHTML = response.data.condition.description;
+  time.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 function cityData(city) {
   let apiKey = "03036dt370ce468aff38bf274ca02od3";
@@ -18,4 +51,4 @@ function searchCity(event) {
 let formSearchElement = document.querySelector("#form-input");
 formSearchElement.addEventListener("submit", searchCity);
 
-cityData("Brussels");
+cityData("Tokyo");
