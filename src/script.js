68f6f336b776f596864f7ba0196d7d8a319a8190
Melba Mokoena <Melmok2@gmail.com>
2024-05-09 00:05:41 +0200

    added forecast api

--- src/script.js
+++ src/script.js
@@ -16,6 +16,8 @@ function updateWeather(response) {
   description.innerHTML = response.data.condition.description;
   time.innerHTML = formatDate(date);
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="city-emoji">`;
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -42,7 +44,7 @@ function formatDate(date) {
 }
 function cityData(city) {
   let apiKey = "03036dt370ce468aff38bf274ca02od3";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
 function searchCity(event) {
@@ -50,7 +52,39 @@ function searchCity(event) {
   let cityInput = document.querySelector("#form-search");
   cityData(cityInput.value);
 }
+function getForecast(city) {
+  let apiKey = "03036dt370ce468aff38bf274ca02od3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function displayForecast(response) {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecast = "";
+
+  days.forEach(function (day) {
+    forecast =
+      forecast +
+      `
+        <div class="column">
+          <div class="forecast-day">${day}</div>
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+            class="emoji-forecast"
+          />
+          <div class="forecast-temperature">
+            <span class="temperature-max">29</span>
+            <span class="max-symbol">°</span>
+            <span class="temperature-min">11</span>
+            <span class="min-symbol">°</span>
+          </div>
+        </div>
+      `;
+  });
+  let forecastElement = document.querySelector(".row");
+  forecastElement.innerHTML = forecast;
+}
 let formSearchElement = document.querySelector(".form-input");
 formSearchElement.addEventListener("submit", searchCity);
 
 cityData("Tokyo");
+displayForecast();
