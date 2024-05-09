324ccb0280b54d48a1aa0232b631d4e02ff8a0d4
Melba Mokoena <Melmok2@gmail.com>
2024-05-09 12:49:07 +0200

    resolved api error and edited css

--- src/script.js
+++ src/script.js
@@ -44,7 +44,7 @@ function formatDate(date) {
 }
 function cityData(city) {
   let apiKey = "03036dt370ce468aff38bf274ca02od3";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
 function searchCity(event) {
